b68be97b8091376c464d3ed25d919bd48654a86e
Kristian Sik <krissik98@yandex.ua>
2018-11-04 22:43:12 +0200

    Implemented checking for graph isomorphism algorithm

---
+++ TravellingSalesmanProblem/TravellingSalesmanProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravellingSalesmanProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new Graph();
+            g.ReadAdjacencyMatrix("1.dat");
+            g.TravellingSalesmanProblem(g.AdjacencyMatrix, 0, new List<Graph.Edge>());
+            Console.ReadKey();
+        }
+    }
+}
